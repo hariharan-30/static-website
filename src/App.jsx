3580d562ffb233d0f3309b4566e071d02fac8751
hariharan-30 <hariharan300901@gmail.com>
2025-04-18 12:45:07 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import HomeService from "./components/HomeService";
-import ServiceCard from "./components/ServiceCard";
-import About from "./components/About";
-import Client from "./components/Client";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Header from "./components/Header";
+// import Navbar from "./components/Navbar";
+// import HomeService from "./components/HomeService";
+// import ServiceCard from "./components/ServiceCard";
+// import About from "./components/About";
+// import Client from "./components/Client";
+// import Contact from "./components/Contact";
+// import Footer from "./components/Footer";
 import LandingPage from "./components/LandingPage"
-import Testing from "./components/Testing";
+// import Testing from "./components/Testing";
 
 
 
@@ -17,22 +17,23 @@ import Testing from "./components/Testing";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route exact path="/header" element={<Header />} />
-        <Route exact path="/landingpage" element={<LandingPage />} />
-        <Route exact path="/navbar" element={<Navbar />} />
-        <Route exact path="/service" element={<HomeService />} />
-        <Route exact path="/card" element={<ServiceCard />} />
-        <Route exact path="/about" element={<About />} />
-        <Route exact path="/client" element={<Client />} />
-        <Route exact path="/contact" element={<Contact />} />
-        <Route exact path="/footer" element={<Footer />} />
-        <Route exact path="/testing" element={<Testing />} />
-  
-        
-      </Routes>
-    </BrowserRouter>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route exact path="/header" element={<Header />} />
+    //     <Route exact path="/landingpage" element={<LandingPage />} />
+    //     <Route exact path="/navbar" element={<Navbar />} />
+    //     <Route exact path="/service" element={<HomeService />} />
+    //     <Route exact path="/card" element={<ServiceCard />} />
+    //     <Route exact path="/about" element={<About />} />
+    //     <Route exact path="/client" element={<Client />} />
+    //     <Route exact path="/contact" element={<Contact />} />
+    //     <Route exact path="/footer" element={<Footer />} />
+    //     <Route exact path="/testing" element={<Testing />} />
+    //   </Routes>
+    // </BrowserRouter>
+    <div>
+      <LandingPage />
+    </div>
   );
 };
 
